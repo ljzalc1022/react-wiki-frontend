e9f37034c6ba30415ae7be5fe04ef0f26910b93b
Lu Jiazhou <2100012965@stu.pku.edu.cn>
2024-08-16 19:30:26 +0800

    use /view/FrontPage as the hompage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom'
 import PageView from './PageView.js'
 import PageEdit from './PageEdit.js'
 
@@ -7,9 +7,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to="/view/FrontPage"/>}></Route>
         <Route path="/view/:title" element={<PageView />}></Route>
         <Route path="/edit/:title" element={<PageEdit />}></Route>
       </Routes>
+      <Link to='/'>Back To Top</Link>
     </BrowserRouter>
   );
 }
